33c608245caf6161696c115a0a584ef4fd04f48a
Leonardo <leonardo.oliveira.esperanca@gmail.com>
2021-05-21 22:00:30 -0400

    Seletor de fragmentos na home com seleção do menu e novos fragmentos em classes Java

--- MinhaCaloria/app/src/main/java/com/example/minhacaloria/HomeActivity.java
+++ MinhaCaloria/app/src/main/java/com/example/minhacaloria/HomeActivity.java
@@ -1,14 +1,47 @@
 package com.example.minhacaloria;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.minhacaloria.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
+    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        selectFragment(new DiaryFragment(), "diary");
+
+        binding.navSidebar.setNavigationItemSelectedListener((MenuItem item) -> {
+            switch (item.getItemId()){
+                case R.id.configuracoes:
+                    selectFragment(new SettingsFragment(), "settings");
+                    break;
+                case R.id.perfil:
+                    selectFragment(new ProfileFragment(), "profile");
+                    break;
+                case R.id.diary:
+                    selectFragment(new DiaryFragment(), "diary");
+                    break;
+            }
+            return true;
+        });
+    }
+
+
+    private void selectFragment(Fragment f, String tag){
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_content, f, tag);
+        transaction.addToBackStack("sttack");
+        transaction.commit();
     }
 }
